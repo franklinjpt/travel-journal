23e5a8637ba5298597e6e7fb9ec0113243685c73
Franklin <franklinpinedat@gmail.com>
2022-08-17 10:36:54 -0500

    navbar completed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <nav className="header-nav">
+      <span className="header-nav__logo"></span>
+      <p className="header-nav__title">My Travel Journal</p>
+    </nav>
+  )
+}
